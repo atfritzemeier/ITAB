7d3b5d50ec9ef3bfee14f2f4305ea1d01d4dd867
Austin Fritzemeier <austinfritzemeierwordpress@gmail.com>
2018-06-27 15:13:50 -0500

    First commit of backup

---
+++ wordpress/wp-content/themes/freak/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package freak
+ */
+?>
+
+	</div><!-- #content -->
+	<?php get_sidebar('footer'); ?>
+<?php get_template_part('subscription'); ?>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			<?php printf( __( 'Designed By %1$s.', 'freak' ), '<a target="blank" href="'.esc_url("https://inkhive.com/").'" rel="nofollow">InkHive</a>' ); ?>
+			<span class="sep"></span>
+			<?php echo ( esc_html(get_theme_mod('freak_footer_text')) == '' ) ? ('&copy; '.date('Y').' '.get_bloginfo('name').__('. All Rights Reserved. ','freak') ) : esc_html( get_theme_mod('freak_footer_text') ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+	
+</div><!-- #page -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
